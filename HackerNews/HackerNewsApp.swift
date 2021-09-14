3a2649755378b5490ac3328d0b9ac3ce12fd414f
wadva <wadva474@gmail.com>
2021-09-14 22:45:49 +0100

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by ABDUL-WADUD MUSA on 14/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
